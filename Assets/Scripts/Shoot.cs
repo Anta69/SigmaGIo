dc99be224ed5d2045dae16512cd7730bf84afd8d
TemukaCoding <temuchintemo@gmail.com>
2025-05-12 06:50:10 -0700

    init

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawnPositions;
+    public float shootForce = 500;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+          GameObject bulletClone = Instantiate(bullet, bulletSpawnPositions.position, Quaternion.identity);
+            bulletClone.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+            bulletClone.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
